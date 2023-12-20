806fb6cf2457da40786a7361e6f60d721af34fd9
clearloop <26088946+clearloop@users.noreply.github.com>
2023-12-20 13:09:58 +0800

    feat(api): check related apis

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,10 @@
 import core from "@actions/core";
 import github from "@actions/github";
 import { GitHub } from '@actions/github/lib/utils';
-import { WorkflowId, WorkflowInputs, WorkflowRun } from "@/types";
+import {
+    CheckRun, Conclusion, Job, Output, Status,
+    WorkflowId, WorkflowInputs, WorkflowRun
+} from "@/types";
 import { wait } from "@/utils";
 
 /**
@@ -22,6 +25,31 @@ export class Api {
         this.owner = owner;
         this.repo = repo;
         this.octokit = github.getOctokit(core.getInput("token"));
+    }
+
+    /**
+     * Create check with provided arguments.
+     *
+     * @param {string} name - the name of the check run.
+     * @param {string} head_sha - creates check on this head sha.
+     * @param {Output} output - the output of the check run.
+     * @returns {CheckRun} - Github check run.
+     */
+    public async createCheck(
+        name: string,
+        head_sha: string,
+        output: Output,
+    ): Promise<CheckRun> {
+        const { data } = await this.octokit.rest.checks.create({
+            owner: this.owner,
+            repo: this.repo,
+            name,
+            head_sha,
+            output,
+        });
+
+        core.debug(`Created check ${data} .`);
+        return data;
     }
 
     /**
@@ -54,6 +82,23 @@ export class Api {
     }
 
     /**
+     * Get a specifed job from a workflow run.
+     *
+     * @param {number} run_id - The workflow run id.
+     * @param {string[]} names - Job names to be filtered out.
+     * @returns {Promise<Job[]>} - Jobs of a workflow run.
+     */
+    public async getJobs(run_id: number, names: string[]): Promise<Job[]> {
+        const { data: { jobs } } = await this.octokit.rest.actions.listJobsForWorkflowRun({
+            owner: this.owner,
+            repo: this.repo,
+            run_id,
+        });
+
+        return jobs.filter((job) => names.includes(job.name));
+    }
+
+    /**
      * List workflow runs for specifed workflow and branch.
      *
      * @param {WorkflowId} workflow_id - The ID of the workflow.
@@ -61,7 +106,7 @@ export class Api {
      * @param {boolean} retry - If keep requesting the result until getting some.
      * @returns {Promise<WorkflowRuns} - Sorted workflow runs.
      */
-    private async latestRun(
+    public async latestRun(
         workflow_id: WorkflowId,
         head_sha?: string,
         retry?: boolean,
@@ -90,5 +135,32 @@ export class Api {
         });
 
         return runs[0];
+    }
+
+
+    /**
+     * Update a check run from jobs.
+     *
+     * @param {Job} job - The most important job of a workflow run.
+     */
+    public async updateCheck(job: Job) {
+        let status: Status = "in_progress";
+        if (job.status === "waiting") {
+            status = undefined;
+        } else {
+            status = job.status;
+        }
+
+        let conclusion: Conclusion = "neutral";
+        if (job.conclusion) {
+            conclusion = job.conclusion;
+        }
+
+        await this.octokit.rest.checks.update({
+            owner: this.owner,
+            repo: this.repo,
+            status,
+            conclusion
+        })
     }
 }
--- src/types.ts
+++ src/types.ts
@@ -4,10 +4,28 @@
 
 import { components, operations } from "@octokit/openapi-types";
 
-// The ID of the workflow. You can also pass the workflow file name as a string.
+/// Check run schema.
+export type CheckRun = components["schemas"]["check-run"];
+
+/// Check run id.
+export type CheckRunId = components["parameters"]["check-run-id"];
+
+/// The status of the check.
+export type Status = operations["checks/create"]["requestBody"]["content"]["application/json"]["status"];
+
+/// The output of the check.
+export type Output = operations["checks/create"]["requestBody"]["content"]["application/json"]["output"];
+
+/// The final conclusion of the check.
+export type Conclusion = operations["checks/create"]["requestBody"]["content"]["application/json"]["conclusion"];
+
+/// Job schema.
+export type Job = components["schemas"]["job"];
+
+/// The ID of the workflow. You can also pass the workflow file name as a string.
 export type WorkflowId = components["parameters"]["workflow-id"];
 
-// Workflow dispatch inputs.
+/// Workflow dispatch inputs.
 export type WorkflowInputs = operations["actions/create-workflow-dispatch"]["requestBody"]["content"]["application/json"]["inputs"];
 
 /// Workflow run schema.
